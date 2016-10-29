26df787a979235d1d6fe849576f5549392c1a84b
Josmil Tito <luizt815@gmail.com>
2016-10-29 13:54:31 -0500

    ultimo Commit de Josmil

--- Demo003/src/pe/egcc/eurekaapp/controller/CuentaController.java
+++ Demo003/src/pe/egcc/eurekaapp/controller/CuentaController.java
@@ -3,7 +3,9 @@ package pe.egcc.eurekaapp.controller;
 import java.util.List;
 import java.util.Map;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import pe.egcc.eurekaapp.service.espec.CuentaServiceEspec;
 import pe.egcc.eurekaapp.service.impl.CuentaServiceImpl;
@@ -27,11 +29,20 @@ public class CuentaController {
   }
   
   public void doConsultar(){
+    FacesMessage message;
     try {
       CuentaServiceEspec service = new CuentaServiceImpl();
       lista = service.getMovimientos(cuenta);
+      if(lista.size() == 0){
+        throw new Exception("Cuenta no existe.");
+      }
+      message = new FacesMessage(FacesMessage.SEVERITY_INFO, 
+          "Proceso ok.", "Cuenta si existe.");
     } catch (Exception e) {
+      message = new FacesMessage(FacesMessage.SEVERITY_ERROR, 
+          "Error en el proceso.", e.getMessage());
     }
+    FacesContext.getCurrentInstance().addMessage(null, message);
   }
   
 }
